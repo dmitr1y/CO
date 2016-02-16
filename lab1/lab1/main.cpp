503039de6c4706ffd84f4084fcf503c573e94cb7
Константинов Дмитрий <aser00707@ya.ru>
2016-02-16 23:20:40 +0300

    add menu

--- lab1/lab1/main.cpp
+++ lab1/lab1/main.cpp
@@ -5,16 +5,67 @@ using namespace std;
 template <typename T>
 void PrintBinNumberf(T);
 void PrintBin(char);
+void printLine(int);
 
 int main()
 {
+	short int choice = 0;
 	int a;
+	bool isExit = 0;
 	char b;
 	float c;
-	float d = 15.3;
+	double d ;
 	long e;
-	cout << "input: ";
-	PrintBinNumberf(d);
+	do
+	{
+		cout << "===============================" << endl
+			<< "+++++++++++++MENU++++++++++++++" << endl
+			<< "==============================" << endl
+			<< "|| " << "1 - int" << endl
+			<< "|| " << "2 - char" << endl
+			<< "|| " << "3 - float" << endl
+			<< "|| " << "4 - double" << endl
+			<< "|| " << "5 - long" << endl
+			<< "|| " << "0 - exit" << endl
+			<< "==============================" << endl;
+		cout << "you choice: ";
+		cin >> choice;
+		cout << "Input ";
+		switch (choice)
+		{
+		case 1:
+			cout << "int: ";
+			cin >> a;
+			PrintBinNumberf(a);
+			break;
+		case 2:
+			cout << "char: ";
+			cin >> b;
+			PrintBinNumberf(b);
+			break;
+		case 3:
+			cout << "float: ";
+			cin >> c;
+			PrintBinNumberf(c);
+			break;
+		case 4:
+			cout << "double: ";
+			cin >> d;
+			PrintBinNumberf(d);
+			break;
+		case 5:
+			cout << "long: ";
+			cin >> e;
+			PrintBinNumberf(e);
+			break;
+		case 0:
+			isExit = 1;
+			break;
+		default:
+			cout << "ERR: wrong choise" << endl;
+			break;
+		}
+	} while (!isExit);
 	system("pause");
 	return 0;
 }
@@ -23,39 +74,39 @@ template<typename T>
 void PrintBinNumberf(const T arg)
 {
 	char *pointer = (char*)&arg;
-
+	printLine(sizeof(T));
+	for (int i =0; i < sizeof(T) * 8 ; i++)
+	{
+		cout << i << "|";
+		if (i<10 && i>=0)
+		{
+			cout << "0";
+		}
+	}
+	printLine(sizeof(T));
 	for (int i = 0; i < sizeof(T); i++, pointer++)
 	{
 		PrintBin(*pointer);
 	}
-	cout << endl;
+	printLine(sizeof(T) );
 };
 
 void PrintBin(char number)
 {
 	for (int i = 0; i < 8; i++)
 	{
-		cout << (number & 0x01);
+		cout <<" "<< (number & 0x01)<<"|";
 		number >>= 1;
 	}
 }
 
-void menu() {
-	short int choice;
-	cout << "==============================" << endl
-		<< "+++++++++++++MENU++++++++++++++"
-		<< "==============================" << endl
-		<< "|| " << "1 - int" << endl
-		<< "|| " << "2 - char" << endl
-		<< "|| " << "3 - float" << endl
-		<< "|| " << "4 - double" << endl
-		<< "|| " << "5 - long" << endl
-		<< "==============================" << endl;
-	cout << "you choice: ";
-	switch (choice)
-	{
-	default:
-		break;
-	}
 
+void printLine(int countByte)
+{
+	cout << endl;
+	for (int i = countByte * 8; i >0; i--)
+	{
+		cout << "---";
+	}
+	cout << endl;
 }
