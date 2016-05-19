97750f14185a4e812ef04caba813ed2ad67432a5
dmitr1y <aser00707@ya.ru>
2016-05-19 23:17:18 +0300

    added mause callback function added keyboard callback function some fix

--- lab3/lab3/main.cpp
+++ lab3/lab3/main.cpp
@@ -9,32 +9,13 @@ GLint Width = 800, Height = 600;
 float ZOOMX = 6.0;
 float ZOOMY = 6.0;
 float dx = M_PI, dy = M_PI;
-//int mouseX=0, mouseY=0;
-//int isPress = 0;
+float *funcArr = nullptr;
 
-//void MouseFunc(int button, int state, int x, int y)
-//{
-//	mouseX=x;
-//	mouseY=y;
-//}
+void MouseMotion(int x, int y)
+{
+	std::cout << "current cursor position: (" << (x-Width/2)/ZOOMX << "," << (Height/2 -y)/ZOOMY << ")\n";
+}
 
-//void mouseDraw()
-//{
-//	glColor3ub(255, 0, 0);
-//	glPointSize(5);
-//	glBegin(GL_POINTS);
-//	if (isPress == 1)
-//	{
-//		std::cout << "ok\n";
-//	}
-//	if (isPress== GLUT_LEFT_BUTTON)
-//	{
-//		glVertex2f(mouseX / ZOOMX, mouseY / ZOOMY);
-//		std::cout << "click\n";
-//	}
-//	glEnd();
-//	glutSwapBuffers();
-//}
 double func(float x)
 {
 	float tmpSin = sin(x / 4);
@@ -107,10 +88,16 @@ void DrawMyFunc()
 {
 	glBegin(GL_POINTS);
 	glColor3ub(255, 0, 0);
-	for (float x = 3 * M_PI / 2; x < 17 * M_PI; x = x + 0.00001)
+	long funcArrLength = 0;
+	if (funcArr == nullptr)
 	{
-		glVertex2f(x, func(x));
+		funcArr = new float[4715476];
+		for (float x = 3 * M_PI / 2; x < 17 * M_PI; x += 0.00001, funcArrLength++)
+			funcArr[funcArrLength] = func(x);
 	}
+		funcArrLength = 0;
+		for (float x = 3 * M_PI / 2; x < 17 * M_PI; x = x + 0.00001, funcArrLength++)
+			glVertex2f(x, funcArr[funcArrLength]);
 	glEnd();
 }
 
@@ -125,7 +112,6 @@ void Display(void)
 	glTranslated(Width / 2/ZOOMX, Height / 2/ZOOMY, 0);
 	glColor3ub(255, 0.0, 0.0);	
 	DrawMyFunc();
-	//mouseDraw();	
 	glPopMatrix();
 	glFlush();
 }
@@ -134,41 +120,45 @@ void Reshape(GLint w, GLint h)
 {
 	Width = w;
 	Height = h;
-
 	glViewport(0, 0, w, h);
-
-
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(0, w, 0, h, -1.0, 1.0);
-
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
 
 void Keyboard(unsigned char key, int x, int y)
 {
-#define ESCAPE '\033'
-
-	if (key == ESCAPE)
+	if (key == VK_ESCAPE)
 	{
-		//exit(0);
-		dx += 1.0; dy += 1.0;
-		std::cout << "zoom dx:" << dx << ", dy:" << dy << '\n';
-		Display();
+		exit(0);
 	}
-	if (key == 'x')
+	if (GetAsyncKeyState('Y') && GetAsyncKeyState(VK_UP))
 	{
-		ZOOMX += 1;
-		std::cout << "zoom X:" << ZOOMX << "\n";
-		Display();
+		ZOOMY++;
+		std::cout << "zoom Y:" << ZOOMY << "\n";		
 	}
-	if (key == 'y')
+	if (GetAsyncKeyState('Y') && GetAsyncKeyState(VK_DOWN))
 	{
-		ZOOMY += 1;
+		ZOOMY--;
 		std::cout << "zoom Y:" << ZOOMY << "\n";
-		Display();
 	}
+	if (GetAsyncKeyState('X') && GetAsyncKeyState(VK_UP))
+	{
+		ZOOMX++;
+		std::cout << "zoom X:" << ZOOMX << "\n";
+	}
+	if (GetAsyncKeyState('X')&& GetAsyncKeyState(VK_DOWN))
+	{
+		ZOOMX--;
+		std::cout << "zoom X:" << ZOOMX << "\n";
+	}
+	if (GetAsyncKeyState(VK_DELETE))
+	{
+		system("cls");
+	}
+	Display();
 }
 
 void main(int argc, char *argv[])
@@ -177,10 +167,10 @@ void main(int argc, char *argv[])
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(Width, Height);
 	glutCreateWindow("lab3-graphic");
-	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClearColor(1.0, 1.0, 1.0, 1.0);	
 	glutReshapeFunc(Reshape);
 	glutKeyboardFunc(Keyboard);
-	//glutMouseFunc(MouseFunc);
+	glutPassiveMotionFunc(MouseMotion);
 	glutPositionWindow(500, 100);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
